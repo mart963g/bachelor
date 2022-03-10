cc4be798bab8b4caba865731baaf0f20e87ed860
mart963g <suunto2110@gmail.com>
2022-03-10 12:53:35 +0100

    fix moveBuffers function, and add ints to keep track of buffer end

--- LZ77/LZ77.cpp
+++ LZ77/LZ77.cpp
@@ -4,11 +4,9 @@ using namespace std;
 
 void LZ77::openFile(string file_name) {
     this->input_file.open(file_name);
-
-    for(int i = 0; i < this->lookahead_buffer_size; i++){
-        this->lookahead_buffer.push_back(this->input_file.get());
-    }
-    this->lookahead_buffer_index++;
+    this->initialiseBuffers();
+    
+    // this->lookahead_buffer_index++;
     cout << this->lookahead_buffer.data();
 
     // for(int i = 0; i < this->lookahead_buffer_size; i++){
@@ -19,19 +17,46 @@ void LZ77::openFile(string file_name) {
     // cout << "Hello World!\n" << file_name << "\n";
 }
 
-// NOT DONE
-/* Move both buffers one char ahead */
-void LZ77::moveBuffers() {
-    if(this->lookahead_buffer_index == this->lookahead_buffer_size || this->history_buffer_index == this->history_buffer_size){
-        this->cleanBuffers();
+void LZ77::initialiseBuffers(int history_size, int lookahead_size) {
+    this->lookahead_buffer_end = lookahead_size;
+    for(int i = this->lookahead_buffer_start; i < this->lookahead_buffer_end; i++){
+        this->lookahead_buffer.push_back(this->input_file.get());
     }
-    // Add char at lookahead buffer index in lookahead buffer, to end of history buffer
-    // Increase both indexes by 1
-    this->lookahead_buffer.push_back(this->input_file.get());
-    this->history_buffer.push_back(this->lookahead_buffer.data()[this->lookahead_buffer_index++]);
-    this->history_buffer_index++;
+    // History buffer is initialised to be empty
+    this->history_buffer_end = 0;
+    /* Insert some code that runs until history buffer is full */
+    while(this->history_buffer_end < history_size - 1) {
+        // Do something and then
+        this->history_buffer_end++;
+    }
+}
+
+/* Move both buffers steps chars ahead */
+void LZ77::moveBuffers(int steps) {
+    for(int i = 0; i < steps; i++) {
+        if(this->history_buffer_end >= this->buffer_max_size){
+            this->cleanBuffers();
+        }
+        // Add new char to end of lookahead buffer, and move char 
+        // at lookahead start to history end.
+        this->lookahead_buffer.push_back(this->input_file.get());
+        this->history_buffer.push_back(this->lookahead_buffer.data()[this->lookahead_buffer_start]);
+        
+        this->incrementIndexes();
+    }
 }
 
 void LZ77::cleanBuffers() {
     cout << "Clean buffers!\n";
+}
+
+void LZ77::setBufferMaxSize(int size) {
+    this->buffer_max_size = size;
+}
+
+void LZ77::incrementIndexes() {
+    this->lookahead_buffer_start++;
+    this->lookahead_buffer_end++;
+    this->history_buffer_start++;
+    this->history_buffer_end++;
 }
--- LZ77/LZ77.h
+++ LZ77/LZ77.h
@@ -9,19 +9,23 @@ using namespace std;
 
 class LZ77 {
     private:
+        int buffer_max_size = 1024;
         vector<char> history_buffer;
-        int history_buffer_size = 10;
-        int history_buffer_index = 0;
+        int history_buffer_end;
+        int history_buffer_start = 0;
         vector<char> lookahead_buffer;
-        int lookahead_buffer_size = 5;
-        int lookahead_buffer_index = 0;
+        int lookahead_buffer_end;
+        int lookahead_buffer_start = 0;
         ifstream input_file;
         ofstream output_file;
-        void moveBuffers();
+        void moveBuffers(int steps = 1);
         void cleanBuffers();
+        void incrementIndexes();
     
     public:
         void openFile(string file_name);
+        void initialiseBuffers(int history_size = 10, int lookahead_size = 5);
+        void setBufferMaxSize(int size);
 };
 
 #endif
