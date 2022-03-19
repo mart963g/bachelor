18da4d821b74620bb84f5ce95bb2549d5c64b643
mart963g <suunto2110@gmail.com>
2022-03-19 16:49:55 +0100

    add fully functional lz77 decompression!

--- LZ77/LZ77DECOMP.cpp
+++ LZ77/LZ77DECOMP.cpp
@@ -9,12 +9,12 @@ void LZ77DECOMP::setBufferMaxSize(int size) {
 void LZ77DECOMP::initialiseDecompression(string file_name) {
     this->input_file.open(file_name, ios::binary);
     auto pos = file_name.find(".lzip");
-    this->output_file.open(file_name.substr(0, pos), ios::app | ios::binary);
+    this->output_file.open(file_name.substr(0, pos), ios::binary);
 }
 
 void LZ77DECOMP::initialiseDecompression(string file_name, string destination_file) {
     this->input_file.open(file_name, ios::binary);
-    this->output_file.open(destination_file, ios::app | ios::binary);
+    this->output_file.open(destination_file, ios::binary);
 }
 
 void LZ77DECOMP::decompressFile(string file_name) {
@@ -40,11 +40,6 @@ void LZ77DECOMP::decompressAfterInitialisation() {
 
 int LZ77DECOMP::readTokenFromInput(struct token &token) {
     token.offset = this->input_file.get();
-    // If there is only the offset, return an error
-    if(this->input_file.eof()) {
-        cout << "ERROR: Unexpected number of bytes in file\n";
-        exit(1);
-    }
     token.length = this->input_file.get();
     return this->input_file.eof() ? 1 : 0;
 }
@@ -53,19 +48,23 @@ void LZ77DECOMP::moveBuffer(int steps) {
     if (this->buffer_end + steps > this->buffer_max_size) {
         this->cleanBuffer();
     }
-    this->buffer_end+= steps;
+    for(int i = 0; i < steps; i++) {
+        this->output_file.put(this->buffer[this->buffer_end]);
+        this->buffer_end++;
+    }
 }
 
 void LZ77DECOMP::writeBytesFromToken(struct token token) {
+    // If the token does not represent a match simply but the data in the buffer and files
     if (token.offset == 0) {
         this->buffer.push_back(token.length);
-        this->output_file.put(token.length);
         this->moveBuffer();
-    } else {
+    } // Otherwise loop over the elements from the offset 
+    else {
         int offset = (int) token.offset;
-        for (int i = 0; i < (int) token.length; i++) {
-            this->buffer.push_back(this->buffer[this->buffer_end - (offset - i)]);
-            this->output_file.put(this->buffer[this->buffer_end - (offset - i)]);
+        for (int i = 0; i < token.length; i++) {
+            // This works because the buffer_end, gets incremented in every iteration
+            this->buffer.push_back(this->buffer[this->buffer_end - offset]);
             this->moveBuffer();
         }   
     }
--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ CC = g++
 CFLAGS = -Wall -g -ggdb
 EXECS = Tests/run
 
-all: clean lz77 run runTests
+all: clean lz77 tests runTests
 
 lz77: LZ77COMP LZ77DECOMP
 
@@ -14,7 +14,7 @@ LZ77DECOMP: LZ77/LZ77DECOMP.cpp
 	@echo "Building LZ77 decompresion..."
 	@$(CC) $(CFLAGS) -c LZ77/LZ77DECOMP.cpp -o LZ77/LZ77DECOMP.o
 
-run: Tests/run.cpp lz77
+tests: Tests/run.cpp lz77
 	@echo "Building Tests..."
 	@$(CC) $(CFLAGS) -o Tests/run LZ77/LZ77COMP.o LZ77/LZ77DECOMP.o Tests/run.cpp -I LZ77
 
